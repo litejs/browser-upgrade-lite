2d19e31a0bd256302486d385b24cfa51a37a119d
Lauri Rooden <lauri@rooden.ee>
2015-03-03 00:16:31 +0200

    Add initial DOMParser
    Recovered from old project and not tested

---
+++ domparser.js
@@ -0,0 +1,40 @@
+
+
+
+/*
+ * @version    1.1.4
+ * @date       2015-03-01
+ * @stability  3 - Stable
+ * @author     Lauri Rooden <lauri@rooden.ee>
+ * @license    MIT License
+ */
+
+
+
+
+!function(window) {
+	if (!window.DOMParser) window.DOMParser = DOMParser
+
+	function DOMParser(){}
+	DOMParser.prototype.parseFromString = window.ActiveXObject ?
+		parseWithActiveX :
+		parseWithXMLHttpRequest
+
+	function parseWithActiveX(str) {
+		var doc = new ActiveXObject("MSXML.DomDocument")
+		doc.loadXML(str)
+		return doc
+	}
+
+	function parseWithXMLHttpRequest(str, mime) {
+		var req = new XMLHttpRequest()
+		req.open("GET", "data:" + (mime || "application/xml") +
+			";charset=utf-8," + encodeURIComponent(str), false)
+
+		if (mime && req.overrideMimeType) req.overrideMimeType(mime)
+		req.send()
+		return req.responseXML
+	}
+
+}(this)
+
